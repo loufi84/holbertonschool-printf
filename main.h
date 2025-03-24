3220f16c268658642959c481388e7ac8b76f79dd
loufi84 <quentin.lataste@gmail.com>
2025-03-24 13:54:23 +0100

    Created the structure and added functions definition in main.h

--- main.h
+++ main.h
@@ -1,7 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _print_str(va_list args);
+
+/**
+ * struct specifier_t - The data structure used to link specifiers to functions
+ *
+ * @specifier: The specifier to check
+ *
+ * @func: The function to used when specifier match
+ */
+
+typedef struct specifier_t
+{
+	char specifier;
+	int (*func)(va_list args);
+} specifier_t;
+
 
 #endif /* MAIN_H */
