a685f91ac9742cb84a305087756b8402260afe44
loufi84 <quentin.lataste@gmail.com>
2025-03-24 14:55:32 +0100

    Created the print_str.c function

--- main.h
+++ main.h
@@ -1,7 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_str(va_list args);
+
+/**
+ * struct specifier_t - A data structure linking specifiers with functions
+ * @specifier: The specifier selected by the user
+ * @func: The function used to format the output
+ */
+
+typedef struct specifier_t
+{
+	char specifier;
+	int (*func)(va_list args);
+} specifier_t;
+
 
 #endif /* MAIN_H */
---
+++ print_str.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_str - Print a string
+ *
+ * Description : This function will take an argument from va_arg in printf
+ *				 and will print the string to stdout
+ *
+ * @args: The string inputed to the function
+ *
+ * Return: The length of the string
+ */
+
+int print_str(va_list args)
+{
+	int len = 0;
+	char *str = va_arg(args, char *);
+
+	while (str[len])
+		len++;
+
+	write(1, str, len);
+
+	return (len);
+}
