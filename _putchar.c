4779a9d87dd77feb7236c2dfc79d8cdd8a431030
alizium <10549@holbertonstudents.com>
2025-03-24 14:17:35 +0100

    Created _puchar.c function

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - prints a single character
+ * @c: character to be printed
+ * Return: a character to standard output (stdout)
+ */
+
+int _putchar(char c)
+{
+return  (write(1, &c, 1));
+}
