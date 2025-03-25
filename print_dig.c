2e66ef7557c94b261b67f5cd07d8142f4cca044d
alizium <10549@holbertonstudents.com>
2025-03-25 10:06:01 +0100

    coded print_dig function

--- print_dig.c
+++ print_dig.c
@@ -4,5 +4,27 @@
 
 int print_dig(va_list args)
 {
+char buffer[12];
+int count = 0, index1 = 0, index2 = 0, n = va_arg(args, int);
+unsigned int num;
 
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+else
+{
+num = n;
+}
+while (num > 0)
+{
+buffer[index1++] = num % 10 + '0';
+num /= 10;
+}
+for (index2 = index1 - 1; index2 >= 0; index2--)
+{
+count += _putchar(buffer[index2]);
+} 
+return (count);
 }
