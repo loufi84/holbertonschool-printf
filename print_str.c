c225c7b530f199640d4433d95a694cb16609a50c
alizium <10549@holbertonstudents.com>
2025-03-26 10:51:37 +0100

    added comments on print str

--- print_str.c
+++ print_str.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+/* Inclusion du fichier d'en-tête contenant les déclarations des fonctions */
+/* Inclusion de la bibliothèque pour gérer les arguments variables */
+/* Inclusion de la bibliothèque pour la fonction write */
 
 /**
  * print_str - Print a string
@@ -15,17 +18,21 @@
 
 int print_str(va_list args)
 {
+/* Compteur pour la longueur de la chaîne */
 	int len = 0;
+/* Récupération de la chaîne depuis la liste d'arguments */
 	char *str = va_arg(args, char *);
-
+/* Si la chaîne est NULL, on remplace par "(null)" */
 	if (str == NULL)
 		str = "(null)";
-
+/* Boucle pour afficher chaque caractère de la chaîne */
 	while (str[len])
 	{
+/* Affichage du caractère courant */
 		_putchar(str[len]);
+/* Incrémentation du compteur de longueur */
 		len++;
 	}
-
+/* Retourne la longueur de la chaîne */
 	return (len);
 }
